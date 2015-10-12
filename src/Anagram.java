7e4e347d5667178bda1fca7116cb5f19f752a0c7
Anmolpreet <anmolpreet_sandhu@hotmail.com>
2015-10-11 22:08:19 -0400

    2nd commit

--- src/Anagram.java
+++ src/Anagram.java
@@ -1,11 +1,34 @@
 import java.util.Arrays;
 
+/*
+ * @author Anmolpreet Sandhu
+ */
+
+
 public class Anagram {
 
-	public static boolean anagram(String[] a) {
-	
-		Arrays.sort(a);
+	public static boolean anagram(String[] a, String[] b) {
 		
+		int l1 = a.length;
+		int l2 = b.length;
+		
+		
+		//if length isn't equal strings can't be anagram
+		if(l1 != l2) {
+			return false;
+		}
+		else {
+			
+			//sort to compare
+			Arrays.sort(a);
+			Arrays.sort(b);
+			
+			//check if they have same characters
+			for (int i = 0; i < l1;  i++)
+			       if (a[i] != b[i])
+			         return false;
+			
+		}
 		return true; 
 	}
 	
@@ -16,13 +39,14 @@ public class Anagram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String[] s ={"a", "r","s","b","c"};
-		String[] t ={"r","m","l","s","c"};
-		if(anagram(s) == anagram(t)) {
-			System.out.println("Yo!");
+		//Just example, no need to hard code otherwis
+		String[] s ={"a", "r","m","y"};
+		String[] t ={"m","a","r","y"};
+		if(anagram(s,t)) {
+			System.out.println("Yes, Strings are anagram");
 		}
 		else
-			System.out.println("Nope!");
+			System.out.println("No, Strings are not amagram");
 
 	}
 
